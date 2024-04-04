219babc34a36a39261f16190f0664a30c9195518
Yeahcold <roganyc@naver.com>
2024-04-04 20:03:58 +0900

    [FEAT] 최근 일주일간 post 조회 기능 추가 [fix] Comment 조회 안 되던 버그 수정 [feat] comment API 추가

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('information', info, name ='info'),
-    path('<int:id>' , get_post_detail, name = "게시글 조회"),
+    path('', post_list, name="post_list"),
+    path('<int:id>', post_detail, name = "post_detail"),
+    path('<int:id>/comment', comment_detail, name = "comment_detail"),
+    path('recent', view_recent_week, name = "recent_week")
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from posts.models import * 
+from datetime import date
+import json
 
 # Create your views here.
 
@@ -53,3 +55,161 @@ def info(request):
             ]
         },
         json_dumps_params={'ensure_ascii': False})
+
+@require_http_methods(["POST", "GET"])
+def post_list(request):
+
+    if request.method == "POST" :
+        body = json.loads(request.body.decode("utf=8"))
+
+        new_post = Post.objects.create(
+            writer = body['writer'],
+            title = body['title'],
+            content = body['content'],
+            category = body['category']
+        )    
+
+        new_post_json = {
+            "id": new_post.id,
+            "writer": new_post.writer,
+            "title" : new_post.title,
+            "content": new_post.content,
+            "category": new_post.category
+        }
+
+        return JsonResponse({
+            'status': 200,
+            'message': '게시글 생성 성공',
+            'data': new_post_json
+        })
+    
+    if request.method == "GET" :
+        post_all = Post.objects.all()
+
+        post_json_all = []
+
+        for post in post_all:
+            post_json = {
+                "id": post.id,
+                "title" : post.title,
+                "writer": post.writer,
+                "content" : post.content,
+                "category": post.category
+            }
+
+            post_json_all.append(post_json)
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 목록 조회 성공",
+            "data" : post_json_all
+        })
+    
+@require_http_methods(["GET", "PATCH", "DELETE"])
+def post_detail(request, id) :
+
+    if request.method == "GET" :
+        post = get_object_or_404(Post, pk = id)
+
+        post_json = {
+                "id": post.id,
+                "title" : post.title,
+                "writer": post.writer,
+                "content" : post.content,
+                "category": post.category
+        }
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "메시지 조회 성공",
+            "data" : post_json
+        })
+    
+    if request.method == "PATCH":
+        body = json.loads(request.body.decode('utf-8'))
+
+        update_post = get_object_or_404(Post, pk = id) #primary key.
+
+        update_post.title = body["title"]
+        update_post.content = body["content"]
+        update_post.category = body["category"]
+
+        update_post.save()
+
+        update_post_json = {
+            "id" : update_post.id,
+            "writer" : update_post.writer,
+            "title" : update_post.title,
+            "content" : update_post.content,
+            "category" : update_post.category
+        }
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 수정 성공",
+            "data" : update_post_json
+        })
+    
+    if request.method == "DELETE":
+        delete_post = get_object_or_404(Post, pk = id)
+        delete_post.delete()
+
+        return JsonResponse({
+                'status': 200,
+                'message': '게시글 삭제 성공',
+                'data': None
+        })
+    
+
+#특정 게시글에 포함된 모든 comment 읽어오는 API 만들기 (특정 게시글이므로 id 필요함.)
+@require_http_methods(["GET"])
+def comment_detail(request, id) :
+
+    if request.method == "GET":
+        post = get_object_or_404(Post, pk=id)
+
+        comment_all = Comment.objects.filter(post_id=post)
+
+        comment_all_json = []
+
+        for comment in comment_all:
+            comment_json = {
+                "id" : comment.id,
+                "post_id" : comment.post_id.id,
+                "user_id" : comment.user_id,
+                "content" : comment.content
+            }
+            comment_all_json.append(comment_json)
+        
+        return JsonResponse({
+            "status" : 200,
+            "message" : "게시글 댓글 목록 조회 성공",
+            "data" : comment_all_json
+        })
+    
+#최근 일주일 동안 작성된 게시글 목록을 볼 수 있는 기능 제작
+@require_http_methods(["GET"])
+def view_recent_week(request):
+    
+    if request.method == "GET":
+
+        recent_all = Post.objects.filter(updated_at__range=(date(2024, 4, 4), date(2024, 4, 10))).order_by('-created_at')
+
+        recent_json_all = []
+
+
+        for recent in recent_all:
+            recent_json = {
+                    "id": recent.id,
+                    "title" : recent.title,
+                    "writer": recent.writer,
+                    "content" : recent.content,
+                    "category": recent.category
+            }
+            recent_json_all.append(recent_json)
+
+        return JsonResponse({
+            "status" : 200,
+            "message" : "일주일간 포스트 조회 성공",
+            "data" : recent_json_all
+        })
